ab30014a5c65cfed403ec9c4197c454433e3df68
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-12-01 01:47:52 -0600

    Add files via upload

---
+++ EVA3_1_METODO.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodo;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA3_1_METODO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO code aplication logic here
+        //invocar el metodo llamada a metodo --> usarlo
+        imprimirMensaje("YO ESTABA ENEL GRUPO DE LA GRASA Y ESTABA HONEANDO UNOS MOMOS Y DEREPENTE SIN AVISAR ME DIERON BAN!");
+        
+     // TODO code application logic here
+        //SON MODIFICADORES 
+        //PUBLIC STATIC INT MAX (INT NUM1,INT NUMM2)
+        //INT RESULTADO
+        //IF(NUM1,MUN2) 
+        //RESULT= NUM1
+        //ELSE
+        //RESULT=NUM2
+        //RETURN RESULT
+    }    
+    //modificadores
+       //valor retorno
+           //nombre
+                 //parametros (opcionales)
+    //SON UTILES PARA REUTILIZAR CODIGO
+     public static void imprimirMensaje(String mensaje){  
+         //PRINTLN ES UN METODO =0
+        System.out.println("tu mensaje PAPU RICO:"+ mensaje);
+        
+     }
+    }
+
+
